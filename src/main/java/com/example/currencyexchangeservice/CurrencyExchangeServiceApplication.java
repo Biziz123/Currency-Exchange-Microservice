73c4194e7b2594c97578d37912804e6d02f7df8a
Gaurav <gauravsinghkhon@gmail.com>
2024-03-29 19:22:12 +0530

    Added Eureka naming server

--- src/main/java/com/example/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/example/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
 
