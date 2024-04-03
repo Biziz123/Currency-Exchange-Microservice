9d166540b2105f3042c4715dc7aefa2fa19c31ca
Gaurav <gauravsinghkhon@gmail.com>
2024-04-03 18:53:28 +0530

    Connected with Zipkin to trace the request

--- src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.example.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 	
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private Environment environment;
 	
@@ -19,6 +24,8 @@ public class CurrencyExchangeController {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,@PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} to {}",from,to);
 		
 //		CurrencyExchange currencyExchange= new CurrencyExchange(1000L,from,to,BigDecimal.valueOf(50));
 		CurrencyExchange currencyExchange=repository.findByFromAndTo(from, to);
